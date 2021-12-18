5d5675aec611fb2809db8b49dbcdb1893a1e200f
lderossis <lderossis@witekio.com>
2021-12-18 01:19:14 +0100

    Added 2021 day 14 second challenge

--- src/2021/14/14.ts
+++ src/2021/14/14.ts
@@ -1,63 +1,109 @@
 import { curry, last, pipe, range } from "ramda";
 
+type Element = string;
+
+type Template = string;
+
+type Pair = string;
+
 interface Insertion {
-  pair: string;
-  insert: string;
+  pair: Pair;
+  insert: Element;
 }
 
-type Template = readonly string[];
-
-type Pair = readonly string[];
+type PairRegistry = { [pair: Pair]: number };
 
 type ElementsCount = { [element: string]: number };
 
 export function extendedPolymerization(data: string) {
   const [template, insertions] = loadData(data);
-  const curriedFilledPairs = curry(fillPairs)(insertions);
 
-  const polymerization = pipe(explode, curriedFilledPairs, merge);
-  const polymerizedTemplate = range(0, 10).reduce(
-    (p, _) => polymerization(p),
-    template
-  );
+  const getResult = (limit: number) => {
+    const pairs = explode(template);
+    const polymerizedPairs = range(0, limit).reduce(
+      (p: PairRegistry, _) => fillPairs(insertions, p),
+      pairs
+    );
 
-  const countAndGetResult = pipe(count, result);
+    return result(countElements(polymerizedPairs, template));
+  };
 
-  console.log(countAndGetResult(polymerizedTemplate));
+  console.log(`[2021, 14-01] Result after 10 steps is ${getResult(10)}`);
+  console.log(`[2021, 14-02] Result after 40 steps is ${getResult(40)}`);
 }
 
-function display(template: Template): Template {
-  const reduced = template.reduce((c, p) => c + p, "");
-  console.log(reduced);
-  return template;
-}
-
-function explode(template: Template): Pair[] {
+function explode(template: Template): PairRegistry {
   return template
-    .slice(0, template.length - 1)
-    .reduce((p: Pair[], _, i) => p.concat([template.slice(i, i + 2)]), []);
+    .substring(0, template.length - 1)
+    .split("")
+    .reduce((p, _, i) => registerPair(extractPair(template, i), 1, p), {});
 }
 
-function merge(pairs: Pair[]): Template {
-  return pairs
-    .reduce((p: Template, c) => p.concat(c.slice(0, c.length - 1)), [])
-    .concat(last(last(pairs)!)!);
-}
-
-function fillPairs(insertions: Insertion[], pairs: Pair[]): Pair[] {
-  return pairs.map((p) => fillPair(insertions, p));
-}
-
-const fillPair = (insertions: Insertion[], pair: Pair): Pair => {
-  const insertion = insertions.find((i) => i.pair == pair[0] + pair[1]);
-  return insertion != undefined ? [pair[0], insertion.insert, pair[1]] : pair;
+const extractPair = (template: Template, index: number): Pair => {
+  return template.slice(index, index + 2);
 };
 
-function count(template: Template): ElementsCount {
-  return template.reduce((p: ElementsCount, c) => {
-    p[c] = p[c] == undefined ? 1 : p[c] + 1;
-    return p;
-  }, {});
+const registerPair = (
+  pair: Pair,
+  count: number,
+  registry: PairRegistry
+): PairRegistry => {
+  registry[pair] = registry[pair] == undefined ? count : registry[pair] + count;
+  return registry;
+};
+
+const unregisterPair = (
+  pair: Pair,
+  count: number,
+  registry: PairRegistry
+): PairRegistry => {
+  registry[pair] -= count;
+  return registry;
+};
+
+function fillPairs(insertions: Insertion[], count: PairRegistry): PairRegistry {
+  const cInsert = curry(insert)(insertions, count);
+  return Object.keys(count).reduce(cInsert, { ...count });
+}
+
+const insert = (
+  insertions: Insertion[],
+  initialRegistry: PairRegistry,
+  currentRegistry: PairRegistry,
+  pair: Pair
+): PairRegistry => {
+  const insertion = insertions.find((i) => i.pair == pair);
+  if (insertion == undefined) return currentRegistry;
+
+  const newPairs = [
+    insertion.pair[0] + insertion.insert,
+    insertion.insert + insertion.pair[1],
+  ];
+
+  const cUnregisterPair = curry(unregisterPair)(
+    insertion.pair,
+    initialRegistry[pair]
+  );
+  return pipe(
+    curry(registerPair)(newPairs[0], initialRegistry[pair]),
+    curry(registerPair)(newPairs[1], initialRegistry[pair]),
+    cUnregisterPair
+  )(currentRegistry);
+};
+
+function countElements(
+  count: PairRegistry,
+  initialTemplate: Template
+): ElementsCount {
+  const c = Object.keys(count)
+    .filter((p) => count[p] > 0)
+    .reduce((p: ElementsCount, c) => {
+      p[c[0]] = p[c[0]] == undefined ? count[c] : p[c[0]] + count[c];
+      return p;
+    }, {});
+
+  c[last(initialTemplate)]++;
+  return c;
 }
 
 function result(count: ElementsCount): number {
@@ -67,8 +113,8 @@ function result(count: ElementsCount): number {
 
 function loadData(data: string): [Template, Insertion[]] {
   const split = data.split("\n");
-  const template = split[0].split("");
-  const insertions = split.slice(2).map((i) => {
+  const template = split[0];
+  const insertions: Insertion[] = split.slice(2).map((i) => {
     const s = i.split(" -> ");
     return { pair: s[0], insert: s[1] };
   });
